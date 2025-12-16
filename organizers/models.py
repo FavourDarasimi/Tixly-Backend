26b7fb782ecc55c563af786dd6ef004ccb773050
Darasimi <darasimi0603@gmail.com>
2025-12-16 01:20:39 +0100

    models

--- organizers/models.py
+++ organizers/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
+# user = get_user_model()
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=255)
+    available_tickets = models.IntegerField()
+    organizer = models.ForeignKey('accounts.User', on_delete=models.CASCADE, related_name='events')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class TicketType(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='ticket_types')
+    available_tickets = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.event.title} - {self.name}'
+
+class Ticket(models.Model):
+    user = models.ForeignKey(user, on_delete=models.CASCADE, related_name='tickets')
+    ticket_type = models.ForeignKey(TicketType, on_delete=models.CASCADE, related_name='tickets')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
 # Create your models here.
